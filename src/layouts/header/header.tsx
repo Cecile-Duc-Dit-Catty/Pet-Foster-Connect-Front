1faff7f58fcfcf5f0804ec7ff93c33ebf39aee09
Cecile-Duc-Dit-Catty <cecile.ducditcatty@gmail.com>
2024-11-22 19:18:03 +0100

    feature: add static burger menu WIP : set the dynamic version to the burger menu

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -1,18 +1,34 @@
-// import React from 'react';
+import React from 'react';
 import styles from './header.module.css';
 // import logo from '../../assets/logo.svg';
-import mobileLogo from '../../assets/MobileLogo.svg';
+import mobileLogo from '../../assets/MobileLogo.png';
 import userIcon from '../../assets/icons/user-solid.svg';
 
 const Header = () => {
+    // Etat de gestion pour le menu Burger
+    const [menuOpen, setMenuOpen] = React.useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen); // Inverse l'état du menu
+    };
+
     return (
         <header className={styles.header}>
-            <img src={mobileLogo} alt="Logo" className="logo" />
-            <img src={userIcon} alt="Mon compte" className="userIcon" />
-            <div className="burger-menu">
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
+            <div className={styles.logoContainer}>
+                <img src={mobileLogo} alt="Logo" className={styles.logo} />
+            </div>
+            <div className={styles.userBurgerContainer}>
+                <div className={styles.userIconContainer}>
+                    <img
+                        src={userIcon}
+                        alt="Mon compte"
+                        className={styles.userIcon}
+                    />
+                </div>
+                <div className={styles.burgerMenu} onClick={toggleMenu}>
+                    <div className={styles.line}></div>
+                    <div className={styles.line}></div>
+                    <div className={styles.line}></div>
+                </div>
             </div>
         </header>
     );
