fb7be44abe24d6f809eaadbe72246c8cf67048bd
Cecile-Duc-Dit-Catty <cecile.ducditcatty@gmail.com>
2024-11-27 23:29:25 +0100

    feature: WIP add some responsivness tablet & dekstop version

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import styles from './HomePage.module.css';
 import Carousel from '../components/carouselAnimalCard/carouselAnimalCard.tsx';
 import DogImg from '../assets/images/dog-with-foster.png';
 import CtaButton from '../components/mainButton/ctaButton.tsx';
+import AnimalCard from '../components/animalCard/animalCard.tsx';
 
 const HomePage = () => {
     return (
@@ -11,13 +12,26 @@ const HomePage = () => {
                     <div className={styles.headerDescription}>
                         <p>Accueillez un animal, devenez famille d’accueil !</p>
                     </div>
+                    <div className={styles.headerDescriptionTablet}>
+                        <p>
+                            Accueillez un animal dans le besoin, devenez famille
+                            d’accueil !
+                        </p>
+                    </div>
                 </div>
                 <section className={styles.carouselSection}>
                     <div className={styles.titleContainer}>
                         <h2>Nouveaux amis en quête d'un foyer</h2>
                         <div className={styles.separator}></div>
                     </div>
-                    <Carousel />
+                    <div className={styles.carouselMobileVersion}>
+                        <Carousel />
+                    </div>
+                    <div className={styles.carouselTabletVersion}>
+                        <AnimalCard />
+                        <AnimalCard />
+                        <AnimalCard />
+                    </div>
                 </section>
                 <section className={styles.ctaSection}>
                     <img
@@ -36,6 +50,35 @@ const HomePage = () => {
                             faites profiter à une animal la chaleur de votre
                             foyer en attendant qu’il trouve sa famille d’accueil
                         </p>
+                    </div>
+                    <CtaButton label="S'inscrire" />
+                </section>
+
+                <section className={styles.ctaSectionTablet}>
+                    <div className={styles.ctaContainer}>
+                        <div className={styles.leftSection}>
+                            <img
+                                src={DogImg}
+                                alt="Chien dans les bras de sa famille d'accueil"
+                            />
+                        </div>
+                        <div className={styles.rightSection}>
+                            <h3>Faisons la différence ensemble !</h3>
+                            <div className={styles.ctaInformation}>
+                                <p>
+                                    Pet Foster Connect aide les associations à
+                                    trouver des familles d’accueil sûres et
+                                    aimante pour les animaux en attente d’un
+                                    foyer définitif
+                                </p>
+                                <p>
+                                    Inscrivez vous en tant que famille d’accueil
+                                    et faites profiter à une animal la chaleur
+                                    de votre foyer en attendant qu’il trouve sa
+                                    famille d’accueil
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     <CtaButton label="S'inscrire" />
                 </section>
